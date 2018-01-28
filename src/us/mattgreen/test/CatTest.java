4f62349a2b85890d4962f6417146fa40a10e6a42
Lucas Nolting <lnolting22@gmail.com>
2018-01-28 00:43:39 -0600

    Fixed tests that were accidentally deleted in merge-LN

--- src/us/mattgreen/test/CatTest.java
+++ src/us/mattgreen/test/CatTest.java
@@ -1,4 +1,25 @@
 package us.mattgreen.test;
+import org.junit.*;
+import us.mattgreen.Cat;
 
 public class CatTest {
+    private Cat Douglas = new Cat(5, "Douglas");
+
+    @Test
+    public void catBuildTest() {
+        Assert.assertNotNull("The constructor is not building a cat properly", Douglas);
+    }
+
+    @Test
+    public void mouseTest() {
+        Assert.assertTrue("The cat has a invalid amount of mice", Douglas.getMousesKilled() > -1);
+    }
+
+    @Test
+    public void addMouseTest() {
+        int mouseTester = Douglas.getMousesKilled() + 1;
+        Douglas.addMouse();
+        Assert.assertEquals("The addMouse() method is defective",
+                mouseTester, Douglas.getMousesKilled());
+    }
 }
